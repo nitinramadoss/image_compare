d799de452bb06d11518188850805d35bfff55335
krlaa <kevindurantony@gmail.com>
2021-05-11 15:56:03 -0400

    base histogram and hash algorithm

--- bin/histogram_algorithm.dart
+++ bin/histogram_algorithm.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 import 'package:image/image.dart';
 
 import 'algorithm.dart';
@@ -5,8 +7,35 @@ import 'algorithm.dart';
 class HistogramAlgorithm implements Algorithm {
   @override
   double compare(Image src1, Image src2) {
-    // TODO: Implement hash algorithm
+    Map<int, int> map1 = {};
+    Map<int, int> map2 = {};
+
+    src1.getBytes().forEach((element) {
+      if (!map1.containsKey(element)) {
+        map1[element] = 1;
+      } else {
+        map1[element] += 1;
+      }
+    });
+    src2.getBytes().forEach((element) {
+      if (!map2.containsKey(element)) {
+        map2[element] = 1;
+      } else {
+        map2[element] += 1;
+      }
+    });
+
+    print('${sortKeys(map1)}\n\n${sortKeys(map2)}');
 
     return 1.0;
+  }
+
+  Map<int, int> sortKeys(Map<int, int> srcMap) {
+    Map<int, int> temp = {};
+    //for sorting based on keys
+    for (var i = 1; i <= 255; i++) {
+      temp[i] = srcMap.containsKey(i) ? srcMap[i] : 0;
+    }
+    return temp;
   }
 }
--- bin/main.dart
+++ bin/main.dart
@@ -1,18 +1,19 @@
 import 'package:image/image.dart';
 
 import 'hash_algorithm.dart';
+import 'histogram_algorithm.dart';
 import 'image_pair.dart';
-import 'dart:io' as Io;
+import 'dart:io' as IO;
 
 void main(List<String> arguments) {
 // NOTE: Test
 
-  var imageFile1 = new Io.File('assets/test1.png').readAsBytesSync();
+  var imageFile1 = new IO.File('assets/test1.png').readAsBytesSync();
   Image image1 = decodeImage(imageFile1);
-  var imageFile2 = new Io.File('assets/test2.png').readAsBytesSync();
+  var imageFile2 = new IO.File('assets/ test2.png').readAsBytesSync();
   Image image2 = decodeImage(imageFile2);
 
   ImagePair pair = ImagePair(image1, image2)
-    ..setAlgorithm(HashAlgorithm())
-    ..compare();
+    ..setAlgorithm(HistogramAlgorithm());
+  print(pair.compare());
 }
