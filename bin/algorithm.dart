4539703d389c2500f6cd20e6b2610f3a9032b106
nitinramadoss <nitinramadoss@gmail.com>
2021-05-13 12:44:43 -0400

    changes to  abstract class to prevent instantiation

--- bin/algorithm.dart
+++ bin/algorithm.dart
@@ -1,6 +1,6 @@
 import 'package:image/image.dart';
 
-class Algorithm {
+abstract class Algorithm {
   // ignore: missing_return
-  double compare(Image src1, Image src2) {}
+  double compare(Image src1, Image src2);
 }
