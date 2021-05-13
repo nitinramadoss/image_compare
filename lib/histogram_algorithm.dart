5564c97b6709bba64151a50343e92647cae3cf88
nitinramadoss <nitinramadoss@gmail.com>
2021-05-13 18:49:19 -0400

    combines rgb histograms and normalizes

--- lib/histogram_algorithm.dart
+++ lib/histogram_algorithm.dart
@@ -7,16 +7,15 @@ import 'package:meta/meta.dart';
 import 'algorithm.dart';
 
 abstract class HistogramAlgorithm extends Algorithm {
-  // Map string to Tuple2(List, List)
+  /// Normalized histograms for [src1] and [src2] stored in a Tuple2
   @protected
-  final histograms = {};
+  var histograms = Tuple2(List.filled(256, 0.0), List.filled(256, 0.0));
 
-  /// Initializes [histograms] for child class compare operations
+  /// Fills color [histograms] for child class compare operations
   @override
   double compare(Image src1, Image src2) {
-    histograms['red'] = Tuple2(List.filled(256, 0), List.filled(256, 0));
-    histograms['blue'] = Tuple2(List.filled(256, 0), List.filled(256, 0));
-    histograms['green'] = Tuple2(List.filled(256, 0), List.filled(256, 0));
+
+    var src1Total = src1.height*src1.width;
 
     for (var i = 0; i < src1.height; i++) {
       for (var j = 0; j < src1.width; j++) {
@@ -24,21 +23,23 @@ abstract class HistogramAlgorithm extends Algorithm {
         var list = Uint32List.fromList([pixel]);
         var byte_data = list.buffer.asUint8List();
 
-        histograms['red'].item1[byte_data[0]] += 1;
-        histograms['blue'].item1[byte_data[1]] += 1;
-        histograms['green'].item1[byte_data[2]] += 1;      
+        histograms.item1[byte_data[0]] += 1/src1Total;
+        histograms.item1[byte_data[1]] += 1/src1Total;
+        histograms.item1[byte_data[2]] += 1/src1Total;     
       }
     }
 
-     for (var i = 0; i < src2.height; i++) {
+    var src2Total = src1.height*src1.width;
+
+    for (var i = 0; i < src2.height; i++) {
       for (var j = 0; j < src2.width; j++) {
         var pixel = src2.getPixelSafe(i, j);
         var list = Uint32List.fromList([pixel]);
         var byte_data = list.buffer.asUint8List();
 
-        histograms['red'].item2[byte_data[0]] += 1;
-        histograms['blue'].item2[byte_data[1]] += 1;
-        histograms['green'].item2[byte_data[2]] += 1;      
+        histograms.item2[byte_data[0]] += 1/src2Total;
+        histograms.item2[byte_data[1]] += 1/src2Total;
+        histograms.item2[byte_data[2]] += 1/src2Total;      
       }
     }
     
