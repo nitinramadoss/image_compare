5cf05013ede1524787628cbc0bb7d7ee0873cfd6
nitinramadoss <nitinramadoss@gmail.com>
2021-05-13 21:51:44 -0400

    Moves chi square class to histogram class file

--- lib/histogram_algorithm.dart
+++ lib/histogram_algorithm.dart
@@ -9,17 +9,21 @@ import 'algorithm.dart';
 abstract class HistogramAlgorithm extends Algorithm {
   /// Number of bins in each histogram
   @protected
-  final binSize = 256;
+  var _binSize;
 
   /// Normalized histograms for [src1] and [src2] stored in a Tuple2
   @protected
-  var histograms;
+  var _histograms;
+
+  /// Default constructor gets implicitly called on sublass instantiation
+  HistogramAlgorithm() {
+    _binSize = 256;
+    _histograms = Tuple2(List.filled(_binSize, 0.0), List.filled(_binSize, 0.0));
+  }
 
   /// Fills color intensity [histograms] for child class compare operations
   @override
-  double compare(Image src1, Image src2) {
-    histograms = Tuple2(List.filled(binSize, 0.0), List.filled(binSize, 0.0));
-    
+  double compare(Image src1, Image src2) {    
     var src1Total = src1.height*src1.width;
 
     for (var i = 0; i < src1.height; i++) {
@@ -28,9 +32,9 @@ abstract class HistogramAlgorithm extends Algorithm {
         var list = Uint32List.fromList([pixel]);
         var byte_data = list.buffer.asUint8List();
 
-        histograms.item1[byte_data[0]] += 1/src1Total;
-        histograms.item1[byte_data[1]] += 1/src1Total;
-        histograms.item1[byte_data[2]] += 1/src1Total;     
+        _histograms.item1[byte_data[0]] += 1/src1Total;
+        _histograms.item1[byte_data[1]] += 1/src1Total;
+        _histograms.item1[byte_data[2]] += 1/src1Total;     
       }
     }
 
@@ -42,12 +46,34 @@ abstract class HistogramAlgorithm extends Algorithm {
         var list = Uint32List.fromList([pixel]);
         var byte_data = list.buffer.asUint8List();
 
-        histograms.item2[byte_data[0]] += 1/src2Total;
-        histograms.item2[byte_data[1]] += 1/src2Total;
-        histograms.item2[byte_data[2]] += 1/src2Total;      
+        _histograms.item2[byte_data[0]] += 1/src2Total;
+        _histograms.item2[byte_data[1]] += 1/src2Total;
+        _histograms.item2[byte_data[2]] += 1/src2Total;      
       }
     }
     
     return 0.0; // default return
+  }
+}
+
+class ChiSquareHistogramAlgorithm extends HistogramAlgorithm {
+  
+  @override
+  double compare(Image src1, Image src2) {
+    // Delegates histogram initialization to parent
+    super.compare(src1, src2);
+
+    var binSize = _binSize;
+
+    var sum = 0.0;
+    for (var i = 0; i < binSize; i++) {
+      var count1 = _histograms.item1[i];
+      var count2 = _histograms.item2[i];
+
+      sum += ((count1-count2) * (count1-count2)) / (count1 + count2);
+    }
+    sum *= 0.5;
+
+    return sum;
   }
 }
