dbd438d2d7277b7a57656445ee58b4bc5595eb93
nitinramadoss <nitinramadoss@gmail.com>
2021-05-14 23:34:25 -0400

    Changes explicit types to var

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,12 @@ import 'dart:io' as IO;
 void main(List<String> arguments) {
 // NOTE: Test
 
-  var imageFile1 = new IO.File('koala1.jpg').readAsBytesSync();
-  Image image1 = decodeImage(imageFile1)!;
-  var imageFile2 = new IO.File('koala2.jpg').readAsBytesSync();
-  Image image2 = decodeImage(imageFile2)!;
+  var imageFile1 = IO.File('koala1.jpg').readAsBytesSync();
+  var image1 = decodeImage(imageFile1)!;
+  var imageFile2 = IO.File('koala2.jpg').readAsBytesSync();
+  var image2 = decodeImage(imageFile2)!;
 
-  ImagePair pair = ImagePair(image1, image2)
-    ..setAlgorithm(ChiSquareHistogramAlgorithm());
+  var pair = ImagePair(image1, image2)
+    ..setAlgorithm(DistanceAlgorithm());
   print(pair.compare());
 }
