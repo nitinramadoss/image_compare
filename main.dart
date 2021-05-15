869131b61b52acf5f75564fdd95f7285539c7ba0
nitinramadoss <nitinramadoss@gmail.com>
2021-05-15 00:03:22 -0400

    moves main.dart entry point to root

---
+++ main.dart
@@ -0,0 +1,18 @@
+import 'package:image/image.dart';
+
+import 'lib/algorithm.dart';
+import 'lib/image_pair.dart';
+import 'dart:io' as IO;
+
+void main(List<String> arguments) {
+// NOTE: Test
+
+  var imageFile1 = IO.File('koala1.jpg').readAsBytesSync();
+  var image1 = decodeImage(imageFile1)!;
+  var imageFile2 = IO.File('koala2.jpg').readAsBytesSync();
+  var image2 = decodeImage(imageFile2)!;
+
+  var pair = ImagePair(image1, image2)
+    ..setAlgorithm(DistanceAlgorithm());
+  print(pair.compare());
+}
