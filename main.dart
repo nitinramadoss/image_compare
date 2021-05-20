27e39e8f439059eb1026c2562d6a93506434c7b1
nitinramadoss <nitinramadoss@gmail.com>
2021-05-20 12:09:21 -0400

    testing in main

--- main.dart
+++ main.dart
@@ -7,12 +7,12 @@ import 'dart:io' as IO;
 void main(List<String> arguments) {
 // NOTE: Test
 
-  var imageFile1 = IO.File('koala1.jpg').readAsBytesSync();
+  var imageFile1 = IO.File('test2.png').readAsBytesSync();
   var image1 = decodeImage(imageFile1)!;
-  var imageFile2 = IO.File('koala2.jpg').readAsBytesSync();
+  var imageFile2 = IO.File('koala1.jpg').readAsBytesSync();
   var image2 = decodeImage(imageFile2)!;
 
   var pair = ImagePair(image1, image2)
-    ..setAlgorithm(ChiSquareHistogramAlgorithm());
+    ..setAlgorithm(IntersectionHistogramAlgorithm());
   print(pair.compare());
 }
