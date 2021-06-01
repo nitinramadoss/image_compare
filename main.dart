90f5607405dec9c7b076fb10f598b02f16321915
nitinramadoss <nitinramadoss@gmail.com>
2021-05-31 20:37:37 -0400

    Moves test images to images directory

--- main.dart
+++ main.dart
@@ -6,9 +6,9 @@ import 'dart:io' as IO;
 void main(List<String> arguments) {
 // NOTE: Test
 
-  var imageFile1 = IO.File('koala1.jpg').readAsBytesSync();
+  var imageFile1 = IO.File('images/white.png').readAsBytesSync();
   var image1 = decodeImage(imageFile1)!;
-  var imageFile2 = IO.File('test2.png').readAsBytesSync();
+  var imageFile2 = IO.File('images/black.png').readAsBytesSync();
   var image2 = decodeImage(imageFile2)!;
 
   var pair = ImagePair(image1, image2)..setAlgorithm(IMED());
