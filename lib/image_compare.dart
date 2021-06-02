1ec6b32fae027d9362c5a17963c65ae2b9f12d71
nitinramadoss <nitinramadoss@gmail.com>
2021-06-02 19:22:07 -0400

    adds list compare function

--- lib/image_compare.dart
+++ lib/image_compare.dart
@@ -12,6 +12,24 @@ double compareImages(Image src1, Image src2, [Algorithm? algorithm]) {
   return algorithm.compare(src1, src2);
 }
 
+/// Compare [target] to each image present in [images] using a
+/// specified [algorithm]. 
+/// Returns a list of doubles corresponding to the compare
+/// output for each pair of input: [target] and images[i].
+/// Output is in the same order as [images].
+/// 
+/// If [algorithm] is not specified, the default (PixelMatching())
+/// will be supplied.
+List<double> listCompare(Image target, List<Image> images, [Algorithm? algorithm]) {
+  algorithm ??= PixelMatching(); //default algorithm
+  var results = <double>[];
+
+  images.forEach((element) => 
+            results.add(compareImages(target, element, algorithm)));
+
+  return results;
+}
+
 /// Abstract class for all algorithms
 abstract class Algorithm {
   /// Tuple of [Pixel] lists for [src1] and [src2]
--- main.dart
+++ main.dart
@@ -9,7 +9,8 @@ void main(List<String> arguments) {
   var imageFile1 = IO.File('images/koala1.jpg').readAsBytesSync();
   var image1 = decodeImage(imageFile1)!;
   var imageFile2 = IO.File('images/koala2.jpg').readAsBytesSync();
-  var image2 = decodeImage(imageFile2)!;
+  var image2 = grayscale(decodeImage(imageFile2)!);
 
-  print(compareImages(image1, image2, PerceptualHash()));
+  print(compareImages(image1, image2, ChiSquareDistanceHistogram()));
+  print(compareImages(image1, image2, IntersectionHistogram()));
 }
