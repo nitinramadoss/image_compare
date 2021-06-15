a7927d56c188bc437894b8029b3faf522b04d52e
Kevin Antony <kevindurantony@gmail.com>
2021-06-15 18:42:41 -0400

    added network capabilites and changed to futures

--- lib/src/functions.dart
+++ lib/src/functions.dart
@@ -1,30 +1,53 @@
+import 'dart:io';
+import 'package:http/http.dart' as http;
 import 'package:image/image.dart';
 import 'algorithms.dart';
 
-/// Compare [src1] and [src2] given a specified [algorithm].
+/// Compare images at [path1] and [path2] with the given a specified [algorithm].
 /// If [algorithm] is not specified, the default (PixelMatching())
 /// will be supplied.
-double compareImages(Image src1, Image src2, [Algorithm? algorithm]) {
+Future<double> compareImages(String path1, String path2,
+    [Algorithm? algorithm]) async {
   algorithm ??= PixelMatching(); //default algorithm
+  Image src1;
+  Image src2;
+  if (RegExp(r"((http|https)://)(www.)?" +
+          "[a-zA-Z0-9@:%._\\+~#?&//=]" +
+          "{2,256}\\.[a-z]" +
+          "{2,6}\\b([-a-zA-Z0-9@:%" +
+          "._\\+~#?&//=]*)")
+      .hasMatch(path1)) {
+    src1 = decodeImage((await http.get(Uri.parse('$path1'))).bodyBytes)!;
+    src2 = decodeImage((await http.get(Uri.parse('$path2'))).bodyBytes)!;
+  } else {
+    src1 = _getImageFile('$path1');
+    src2 = _getImageFile('$path2');
+  }
 
   return algorithm.compare(src1, src2);
 }
 
-/// Compare [target] to each image present in [images] using a
+Image _getImageFile(String path) {
+  var imageFile1 = File(path).readAsBytesSync();
+
+  return decodeImage(imageFile1)!;
+}
+
+/// Compare [targetPath] to each image present in [imagePaths] using a
 /// specified [algorithm].
-/// Returns a list of doubles corresponding to the compare
-/// output for each pair of input: [target] and images[i].
-/// Output is in the same order as [images].
+/// Returns a Future list of doubles corresponding to the compare
+/// output for each pair of input: [targetPath] and imagePaths[i].
+/// Output is in the same order as [imagePaths].
 ///
 /// If [algorithm] is not specified, the default (PixelMatching())
+/// If [type] is not specified, the default (ImageType.asset)
 /// will be supplied.
-List<double> listCompare(Image target, List<Image> images,
-    [Algorithm? algorithm]) {
+Future<List<double>> listCompare(String targetPath, List<String> imagePaths,
+    [Algorithm? algorithm]) async {
   algorithm ??= PixelMatching(); //default algorithm
   var results = <double>[];
-
-  images.forEach(
-      (element) => results.add(compareImages(target, element, algorithm)));
-
+  await Future.wait(imagePaths.map((input) async {
+    results.add(await compareImages(targetPath, input, algorithm));
+  }));
   return results;
 }
