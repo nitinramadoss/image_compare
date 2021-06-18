71858cc978c5229cd08b5b52997df357f7f460e4
nitinramadoss <nitinramadoss@gmail.com>
2021-06-17 22:52:04 -0400

    updates docs, refactors main, adds required params

--- example/main.dart
+++ example/main.dart
@@ -1,29 +1,46 @@
+import 'dart:io';
+
 import 'package:image_compare/image_compare.dart';
 
 void main(List<String> arguments) async {
-  var otherPath = '../images/animals/komodo.jpg';
-  var targetPath = '../images/animals/koala.jpg';
+  var url1 =
+      'https://www.tompetty.com/sites/g/files/g2000007521/f/sample_01.jpg';
+  var url2 =
+      'https://fujifilm-x.com/wp-content/uploads/2019/08/x-t30_sample-images03.jpg';
 
+  // Calculate chi square histogram distance between two network images
   var networkResult = await compareImages(
-    Uri.parse(
-        'https://www.tompetty.com/sites/g/files/g2000007521/f/sample_01.jpg'),
-    Uri.parse(
-        'https://fujifilm-x.com/wp-content/uploads/2019/08/x-t30_sample-images03.jpg'),
-  );
+      src1: Uri.parse(url1),
+      src2: Uri.parse(url2),
+      algorithm: ChiSquareDistanceHistogram());
 
   print('Difference: ${networkResult * 100}%');
 
+  var otherFile = File('../images/drawings/kolam1.png');
+  var targetFile = File('../images/drawings/scribble1.png');
+
+  // Calculate IMED between two asset images
   var assetResult = await compareImages(
-    otherPath,
-    targetPath,
-  );
+      src1: targetFile, src2: otherFile, algorithm: IMED(boxPercentage: 0.001));
+
   print('Difference: ${assetResult * 100}%');
 
   var assetImages = [
-    '../images/animals/deer.jpg',
-    '../images/animals/bunny.jpg',
-    '../images/animals/tiger.jpg'
+    File('../images/animals/deer.jpg'),
+    File('../images/animals/bunny.jpg'),
+    File('../images/animals/tiger.jpg')
   ];
+
+  // Calculate perceptual hash difference between an asset image 
+  // and a list of asset iamges
+  var assetResults = await listCompare(
+    targetSrc: targetFile,
+    srcList: assetImages,
+    algorithm: PerceptualHash(),
+  );
+
+  assetResults.forEach((e) => print('Difference: ${e * 100}%'));
+
   var networkImages = [
     Uri.parse(
         'https://www.tompetty.com/sites/g/files/g2000007521/f/sample_01.jpg'),
@@ -35,20 +52,13 @@ void main(List<String> arguments) async {
         'https://c.files.bbci.co.uk/12A9B/production/_111434467_gettyimages-1143489763.jpg'),
   ];
 
-  // Calculate median hashes between target (src1) and list of images
-  var results = await listCompare(
-    targetPath,
-    assetImages,
-    algorithm: PerceptualHash(),
+  // Calculate average hash difference between a network image
+  // and a list of network images
+  var networkResults = await listCompare(
+    targetSrc: Uri.parse(url1),
+    srcList: networkImages,
+    algorithm: AverageHash(),
   );
 
-  results.forEach((e) => print('Difference: ${e * 100}%'));
-
-  // results = await listCompare(
-  //     'https://c.files.bbci.co.uk/12A9B/production/_111434467_gettyimages-1143489763.jpg',
-  //     networkImages,
-  //     algorithm: MedianHash(),
-  //     type: SourceType.network);
-
-  // results.forEach((e) => print('Difference: ${e * 100}%'));
+  networkResults.forEach((e) => print('Difference: ${e * 100}%'));
 }
